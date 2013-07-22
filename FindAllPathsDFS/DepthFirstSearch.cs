496764da4b198bcd5b14306efd78a22fbb393b64
stanevplamen <stanev.plamen@gmail.com>
2013-07-22 14:14:39 +0300

    BFS DFS

---
+++ FindAllPathsDFS/DepthFirstSearch.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DFSExample
+{
+    class DFSExample // проверава всички
+    {
+        static char[,] matrix = new char[7, 7]
+        {
+            {' ',' ',' ','X','X','X',' '},
+            {' ','X',' ',' ',' ','X',' '},
+            {' ','X',' ','X','X','X',' '},
+            {' ','X',' ',' ',' ',' ',' '},
+            {' ','X',' ','X','X','X',' '},
+            {' ','X','X','X','X','X',' '},
+            {'E',' ',' ',' ',' ',' ',' '},
+        };
+
+        static void PrintMatrix()
+        {
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    if (matrix[i, j] == '*')
+                    {
+                        Console.ForegroundColor = System.ConsoleColor.Red;
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = System.ConsoleColor.White;
+                    }
+                    Console.Write(matrix[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+        static bool InRange(int row, int col)
+        {
+            bool rowInRange = row >= 0 && row < matrix.GetLength(0);
+            bool colInRange = col >= 0 && col < matrix.GetLength(1);
+            return rowInRange && colInRange;
+        }
+
+        static void FindPathToExit(int row, int col)
+        {
+            if (!InRange(row, col))
+            {
+                return;
+            }
+
+            if (matrix[row, col] == 'E')
+            {
+                PrintMatrix();
+            }
+
+            if (matrix[row, col] != ' ')
+            {
+                return;
+            }
+
+            matrix[row, col] = '*';
+            FindPathToExit(row, col - 1); // left
+            FindPathToExit(row - 1, col); // up
+            FindPathToExit(row, col + 1); // right
+            FindPathToExit(row + 1, col); // down
+            matrix[row, col] = ' ';
+        }
+
+        static void Main(string[] args)
+        {
+            FindPathToExit(0, 6);
+        }
+    }
+}
---
+++ FindShortestPathBFS/BreadthFirstSearch.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BreadthFirstSearchExample
+{
+    class BFSExample 
+    {
+        class Point
+        {
+            public int xCoordinate { get; set; }
+            public int yCoordinate { get; set; }
+
+            public Point(int x, int y)
+            {
+                xCoordinate = x;
+                yCoordinate = y;
+            }
+        }
+
+        static char[,] matrix = new char[7, 7]
+        {
+            {' ',' ',' ','X','X','X','S'},
+            {' ','X',' ',' ',' ','X',' '},
+            {' ','X',' ','X','X','X',' '},
+            {' ','X',' ',' ',' ',' ',' '},
+            {' ','X',' ','X','X','X',' '},
+            {' ','X','X','X','X','X',' '},
+            {'E',' ',' ',' ',' ',' ',' '},
+        };
+
+        static void BFS(Point startPoint)
+        {
+            Queue<Point> matrixPoints = new Queue<Point>();
+            matrixPoints.Enqueue(startPoint);
+            while (true)
+            {
+                Point currentPoint = matrixPoints.Dequeue();
+                if (currentPoint.xCoordinate - 1 >= 0)
+                {
+                    if (matrix[currentPoint.xCoordinate - 1, currentPoint.yCoordinate] == 'E')
+                    {
+                        Console.WriteLine("We have found the shortest exit");
+                        return;
+                    }
+                    if (matrix[currentPoint.xCoordinate - 1, currentPoint.yCoordinate] == ' ')
+                    {
+                        matrix[currentPoint.xCoordinate - 1, currentPoint.yCoordinate] = '*';
+                        matrixPoints.Enqueue(new Point(currentPoint.xCoordinate - 1, currentPoint.yCoordinate));
+                    }
+                }
+                if (currentPoint.xCoordinate + 1 < matrix.GetLength(0))
+                {
+                    if (matrix[currentPoint.xCoordinate + 1, currentPoint.yCoordinate] == 'E')
+                    {
+                        Console.WriteLine("We have found the shortest exit");
+                        return;
+                    }
+                    if (matrix[currentPoint.xCoordinate + 1, currentPoint.yCoordinate] == ' ')
+                    {
+                        matrix[currentPoint.xCoordinate + 1, currentPoint.yCoordinate] = '*';
+                        matrixPoints.Enqueue(new Point(currentPoint.xCoordinate + 1, currentPoint.yCoordinate));
+                    }
+                }
+                if (currentPoint.yCoordinate + 1 < matrix.GetLength(0)) 
+                { 
+                    if (matrix[currentPoint.xCoordinate, currentPoint.yCoordinate + 1] == 'E') 
+                    {
+                        Console.WriteLine("We have found the shortest exit"); 
+                        return; 
+                    }
+                    if (matrix[currentPoint.xCoordinate, currentPoint.yCoordinate + 1] == ' ') 
+                    { 
+                        matrix[currentPoint.xCoordinate, currentPoint.yCoordinate + 1] = '*'; 
+                        matrixPoints.Enqueue(new Point(currentPoint.xCoordinate, currentPoint.yCoordinate + 1)); 
+                    } 
+                } 
+                if (currentPoint.yCoordinate - 1 >= 0)
+                {
+                    if (matrix[currentPoint.xCoordinate, currentPoint.yCoordinate - 1] == 'E')
+                    {
+                        Console.WriteLine("We have found the shortest exit");
+                        return;
+                    }
+                    if (matrix[currentPoint.xCoordinate, currentPoint.yCoordinate - 1] == ' ')
+                    {
+                        matrix[currentPoint.xCoordinate, currentPoint.yCoordinate - 1] = '*';
+                        matrixPoints.Enqueue(new Point(currentPoint.xCoordinate, currentPoint.yCoordinate - 1));
+                    }
+                }
+                Console.WriteLine();
+                Console.WriteLine();
+                PrintMatrix();
+            }
+        }
+
+        static void PrintMatrix()
+        {
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    if (matrix[i, j] == '*')
+                    {
+                        Console.ForegroundColor = System.ConsoleColor.Red;
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = System.ConsoleColor.White;
+                    }
+                    Console.Write(matrix[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Point startPoint = new Point(0, 6);
+            BFS(startPoint);
+        }
+    }
+}
