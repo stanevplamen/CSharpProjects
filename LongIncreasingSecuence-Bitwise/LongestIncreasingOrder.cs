734f8867cca37a91ac12a62f06ff7e963676a8eb
stanevplamen <stanev.plamen@gmail.com>
2013-07-22 13:16:38 +0300

    add programs

---
+++ LongIncreasingSecuence-Bitwise/LongestIncreasingOrder.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LongestIncreasingOrder
+{
+    static void Main()
+    {
+        List<int> nArray = new List<int>() { 1, 2, 3, 5, 4, 4, 2, 4, 6, 7, 23, 4, 2 };
+
+        int counter = 0;
+        int maxRepeatCounter = -1;
+        int lastRepeatCounter = int.MinValue;
+        int index = 0;
+        int lastIndex = 0;
+        int currentNumber = int.MinValue;
+        List<int> intTempList = new List<int>();
+        List<int> numbersList = new List<int>();
+        int iterationsNumber = (int)Math.Pow((double)2, nArray.Count);
+
+        for (int i = 1; i <= (iterationsNumber - 1); i++)
+        {
+            currentNumber = int.MinValue;
+            for (int j = 0; j < nArray.Count; j++)
+            {
+                int mask = 1 << j;
+                int nAndMask = mask & i;
+                int bit = nAndMask >> j;
+
+                if (bit == 1)
+                {
+                    if (nArray[j] >= currentNumber)
+                    {
+                        counter++;
+                        intTempList.Add(nArray[j]);
+                        if (maxRepeatCounter < counter)
+                        {
+                            maxRepeatCounter = counter;
+                            index = j - 1;
+                        }
+                    }
+                    else if (nArray[j] < currentNumber)
+                    {
+                        if (maxRepeatCounter < counter)
+                        {
+                            maxRepeatCounter = counter;
+                            index = j - 1;
+                        }
+                        counter = 0;
+                    }
+                    currentNumber = nArray[j];
+                }
+            }
+            if (lastRepeatCounter < maxRepeatCounter)
+            {
+                lastRepeatCounter = maxRepeatCounter;
+                lastIndex = index;
+                numbersList = new List<int>(intTempList);
+                intTempList = new List<int>();
+                counter = 0;
+            }
+            else
+            {
+                intTempList = new List<int>();
+                counter = 0;
+            }
+        }
+
+        Console.WriteLine("The entered array is: ");
+        foreach (var item in nArray)
+        {
+            Console.Write(item + " ");
+        }
+        Console.WriteLine();
+        Console.WriteLine(new string('-', 30));
+
+        Console.WriteLine("The longest increasing order is: ");
+        foreach (var item in numbersList)
+        {
+            Console.Write(item + " ");
+        }
+        Console.WriteLine();
+    }
+}
---
+++ LongIncreasingSecuence-DP/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+public static class LongestIncreasingSecuence
+{
+    static void Main()
+    {
+        int arrayLength = 13;
+        int[] inputNumbers = { 3, 5, 4, 7, 6, -34, 8, 434, -34, 455, 0, 2, 4};
+        int maxLength = 1;
+        int bestEnd = 0;
+        int[] arrayPlus = new int[arrayLength];
+        int[] arrayMinus = new int[arrayLength];
+        arrayPlus[0] = 1;
+        arrayMinus[0] = -1;
+        for (int i = 1; i < arrayLength; i++)
+        {
+            arrayPlus[i] = 1;
+            arrayMinus[i] = -1;
+            for (int j = i - 1; j >= 0; j--)
+            {
+                if (arrayPlus[j] + 1 > arrayPlus[i] && inputNumbers[j] < inputNumbers[i])
+                {
+                    arrayPlus[i] = arrayPlus[j] + 1;
+                    arrayMinus[i] = j;
+                }
+            }
+            if (arrayPlus[i] > maxLength)
+            {
+                bestEnd = i;
+                maxLength = arrayPlus[i];
+            }
+        }
+        Console.Write("Max length: " + maxLength);
+        Console.Write("\n");
+        Console.Write("Sequence end index: " + bestEnd);
+        Console.Write("\n");
+        Console.Write("Longest subsequence: ");
+        int currentBE = bestEnd;
+        int[] longestIncreasingSecuenceArray = new int[arrayLength];
+        int p = 0;
+        while (currentBE != -1)
+        {
+            longestIncreasingSecuenceArray[p] = inputNumbers[currentBE];
+            currentBE = arrayMinus[currentBE];
+            p++;
+        }
+
+        for (int i = longestIncreasingSecuenceArray.Length - 1; i >= 0; i--)
+        {
+            if (longestIncreasingSecuenceArray[i] != 0)
+            {
+                Console.Write("{0} ", longestIncreasingSecuenceArray[i]);
+            }
+        }
+        Console.Write("\n");
+    }
+}
