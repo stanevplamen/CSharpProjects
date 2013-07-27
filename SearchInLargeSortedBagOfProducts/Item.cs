f614df9a2c58c90e614229915cec68dcc4ad5f59
stanevplamen <stanev.plamen@gmail.com>
2013-07-27 13:12:42 +0300

    add a search with Range in SortedBag

---
+++ SearchInLargeSortedBagOfProducts/Item.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Item : IComparable<Item>
+{
+    private string name;
+    private decimal price;
+
+    public string Name
+    {
+        get { return this.name; }
+        private set { }
+    }
+
+    public decimal Price
+    {
+        get { return this.price; }
+        private set { }
+    }
+
+    public Item(string name, decimal price)
+    {
+        this.name = name;
+        this.price = price;
+    }
+
+    public int CompareTo(Item oderItem)
+    {
+        return this.price.CompareTo(oderItem.price);
+    }
+}
